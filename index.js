4a30d56a84a4ed5beeae38d6368250dcbc14ce9c
ksalles <ksalles@dialoginsight.com>
2020-11-10 13:51:19 -0500

    1

--- index.js
+++ index.js
@@ -32,31 +32,9 @@ app.post('/webhook', (req, res) => {
 });
 
 // Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
-
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "EAAD7lc8fKIoBAPCZAXJvkMmFNmbaZBnV2ojqpsnjo2thXqMk0syHlcCkiKp2587xBEgSt7JFhlsFzapOadGQQHpV0g5zhVtQ0W1vsN2bvENu25dr7gSy4eS8pRHKPSnZB7NEvzP4XFUQ8bhQBSQw5llGkD88kmTn4WzGChsuAZDZD"
-        
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-        
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-
-        // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-        
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
-        
-        } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
-        }
-    }
+app.get('/image', (req, res) => {
+    var path = __dirname + '\\images\\image.gif';
+    res.sendFile(path);
 });
 
 // Sets server port and logs message on success
