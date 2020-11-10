bfbec17a761a84515a2f4214ebcad3e411a09a7e
ksalles <ksalles@dialoginsight.com>
2020-11-10 14:49:25 -0500

    dsg

--- index.js
+++ index.js
@@ -7,39 +7,30 @@ const
   app = express().use(bodyParser.json());
 
 
-app.post('/webhook', (req, res) => {  
-
-    let body = req.body;
-
-    // Checks this is an event from a page subscription
-    if (body.object === 'page') {
-
-        // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach(function(entry) {
-
-        // Gets the message. entry.messaging is an array, but 
-        // will only ever contain one message, so we get index 0
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
-        });
-
-        // Returns a '200 OK' response to all requests
-        res.status(200).send('EVENT_RECEIVED');
-    } else {
-        // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
-    }
-
-});
-
 // Adds support for GET requests to our webhook
 app.get('/image', (req, res) => {
     console.log("Call 1 ...");
+    console.log("Resquest header");
     console.log(req['headers']);
+
+    res.set("Accept-CH", "UA-Full-Version, UA-Platform, UA-Platform-Version, UA-Arch, UA-Model");
+
+    console.log("Response header");
+    console.log(res['headers']);
+
+    res.redirect('/image2');
+});
+
+app.get('/image2', (req, res) => {
+    console.log("Call 2 ...");
+    console.log("Resquest header");
+    console.log(req['headers']);
+    
     res.json({ 
         request: req['headers']
       });
 });
+
 
 console.log('View port : ' + process.env.PORT);
 
